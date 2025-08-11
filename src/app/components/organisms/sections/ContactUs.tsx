78e2d429ee3046ab374fae697eb71d086b2bfa02
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2025-08-11 20:14:11 +0700

    Removed redundant exports

--- src/app/components/organisms/sections/ContactUs.tsx
+++ src/app/components/organisms/sections/ContactUs.tsx
@@ -35,5 +35,3 @@ export const ContactUs: React.FC = () => {
     </>
   );
 };
-
-export default ContactUs;
--- src/app/components/organisms/sections/WhatWeSeek.tsx
+++ src/app/components/organisms/sections/WhatWeSeek.tsx
@@ -67,5 +67,3 @@ export const WhatWeSeek: React.FC = () => {
     </>
   );
 };
-
-export default WhatWeSeek;
