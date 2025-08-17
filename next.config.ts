5f03575d3caa8b19d9c9d28cda5bb48992423515
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2025-08-17 14:37:08 +0700

    Added SPA router fallback

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
   images: {
     remotePatterns: [new URL('https://unsplash.com/photos/**'), new URL('https://plus.unsplash.com/**')],
+    unoptimized: true
   },
-  output: 'export'
+  output: "export"
 };
 
 export default nextConfig;
