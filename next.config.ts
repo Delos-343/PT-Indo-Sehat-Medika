2bc5aff34ba50dde1f1a0436868af02701ece9b9
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2025-08-17 13:58:43 +0700

    Setting up deploy config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [new URL('https://unsplash.com/photos/**'), new URL('https://plus.unsplash.com/**')],
-  }
+    unoptimized: true,
+  },
+  output: 'export',
 };
 
 export default nextConfig;
