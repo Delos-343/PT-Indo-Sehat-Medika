8cb41d696dab5d36c368fa45b1dd4c2491d0d09f
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2025-08-17 14:03:23 +0700

    Checking errors

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [new URL('https://unsplash.com/photos/**'), new URL('https://plus.unsplash.com/**')],
-    unoptimized: true,
   },
-  output: 'export',
+  output: 'export'
 };
 
 export default nextConfig;
