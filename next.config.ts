b6284833ca294ec83cbb40c5e438d3ccb8771106
Delos-343 <87126965+Delos-343@users.noreply.github.com>
2025-08-17 13:50:53 +0700

    exporting config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   images: {
     remotePatterns: [new URL('https://unsplash.com/photos/**'), new URL('https://plus.unsplash.com/**')],
   }
